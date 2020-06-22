1047b16544bcafe1c47b7c31cf3f2871833ca53a
misatotoyama <misato030306@gmail.com>
2020-06-22 20:22:37 +0900

    #002

--- js/main.js
+++ js/main.js
@@ -1,10 +1,20 @@
 'use strict';
 
 {
-  // const score1 = 80;
-  // const score2 = 90;
-  // const score3 = 40;
+    // const score1 = 80;
+    // const score2 = 90;
+    // const score3 = 40;
 
-  const scores = [80, 90, 40];
-  console.log(scores);
+    const scores = [80, 90, 40];
+    console.log(scores);
+}
+
+{
+    const scores2 = [80, 90, 40];
+    // console.log(scores2[1]);
+
+    scores2[2] = 44;
+    console.log(scores2);
+
+    console.log(scores2.length);
 }
