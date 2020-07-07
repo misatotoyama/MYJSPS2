5ab01279109143abe42a70c5914509b04053d5d7
misatotoyama <misato030306@gmail.com>
2020-07-07 16:01:13 +0900

    #12

--- js/main.js
+++ js/main.js
@@ -89,3 +89,38 @@
     const updatedPrices = prices.map(price => price + 20);
     console.log(updatedPrices);
 }
+{
+    const numbers = [1, 4, 7, 8, 10];
+
+    const evenNumbers = numbers.filter(number => {
+        if (number % 2 === 0){
+            return true;
+        }else{
+            return false;
+        }
+    });
+
+    console.log(evenNumbers);
+}
+{
+    const point = {
+        x: 100,
+        y: 180,
+    };
+    console.log(point);
+}
+{
+    const point = {
+        x: 100,
+        y: 180,
+    };
+
+    point['x'] =120;
+
+    // console.log(point.x);
+    // console.log(point['y']);
+
+    point.z = 90;
+    delete point.y;
+    console.log(point);
+}
