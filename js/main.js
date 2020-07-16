61a0b224345b49c70838a57eb71dccb17ca0b2fd
misatotoyama <misato030306@gmail.com>
2020-07-16 13:22:45 +0900

    #016

--- js/main.js
+++ js/main.js
@@ -26,7 +26,7 @@
     // console.log(`Score: ${scores[1]}`);
     // console.log(`Score: ${scores[2]}`);
 
-    for (let i = 0; i < scores.length; i++){
+    for (let i = 0; i < scores.length; i++) {
         console.log(`Score ${i}: ${scores[i]}`);
     }
 }
@@ -35,8 +35,8 @@
     scores.push(60, 50);
     scores.shift();
 
-    
-    for (let i = 0; i < scores.length; i++){
+
+    for (let i = 0; i < scores.length; i++) {
         console.log(`Score ${i}: ${scores[i]}`);
     }
 }
@@ -44,16 +44,16 @@
     const scores = [80, 90, 40, 70];
     scores.splice(1, 0, 30, 50);
 
-    for (let i = 0; i < scores.length; i++){
+    for (let i = 0; i < scores.length; i++) {
         console.log(`Score ${i}: ${scores[i]}`);
     }
 }
 {
     const otherScores = [10, 20, 30];
-    const scores =[80, 90, 40, 70, ...otherScores];
+    const scores = [80, 90, 40, 70, ...otherScores];
     // console.log(scores);
 
-    function sum(a, b, c){
+    function sum(a, b, c) {
         console.log(a + b + c);
     }
 
@@ -79,7 +79,7 @@
 {
     const scores = [80, 90, 40, 70];
 
-    scores.forEach((score, index) =>{
+    scores.forEach((score, index) => {
         console.log(`Score:  ${index}; ${score}`);
     });
 }
@@ -93,9 +93,9 @@
     const numbers = [1, 4, 7, 8, 10];
 
     const evenNumbers = numbers.filter(number => {
-        if (number % 2 === 0){
+        if (number % 2 === 0) {
             return true;
-        }else{
+        } else {
             return false;
         }
     });
@@ -115,7 +115,7 @@
         y: 180,
     };
 
-    point['x'] =120;
+    point['x'] = 120;
 
     // console.log(point.x);
     // console.log(point['y']);
@@ -123,4 +123,68 @@
     point.z = 90;
     delete point.y;
     console.log(point);
+}
+{
+    const otherProps = {
+        r: 4,
+        color: 'blue',
+    };
+
+    const point = {
+        x: 100,
+        y: 180,
+        ...otherProps,
+    };
+    console.log(point);
+}
+{
+    const otherProps = {
+        r: 4,
+        color: 'blue',
+    };
+    const point = {
+        x: 100,
+        y: 180,
+        ...otherProps,
+    };
+
+    const { x, r, ...others } = point;
+    console.log(x);
+    console.log(r);
+    console.log(others);
+}
+{
+    const point = {
+        x: 100,
+        y: 180,
+    }
+
+    const keys = Object.keys(point);
+    keys.forEach(key => {
+        console.log(`Key: ${key}`)
+    });
+}
+{
+    let x = 1;
+    let y = x;
+    x = 5;
+
+    console.log(x);
+    console.log(y);
+}
+{
+    let x = [1, 2];
+    let y = x;
+    x[0] = 5;
+
+    console.log(x);
+    console.log(y);
+}
+{
+    let x = [1, 2];
+    let y = [...x];
+    x[0] = 5;
+
+    console.log(x);
+    console.log(y);
 }
